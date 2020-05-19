4aba5dbd25538ff22fab54018fce940d9fa1df22
Douglas Rauber <douglas.rauber@gmail.com>
2020-05-19 06:55:09 -0300

    APP com estrutura para escalar Responsabilidades divididas

--- src/modules/appointments/infra/http/routes/appointements.routes.ts
+++ src/modules/appointments/infra/http/routes/appointements.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -16,23 +14,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-  const appointmentsRepository = new AppointmentsRepository();
-
-  const { provider_id, date } = request.body;
-
-  // Transformação de dado não deve ser repassada para o service
-  const parsedDate = parseISO(date);
-
-  const createAppointmentService = new CreateAppointmentService(
-    appointmentsRepository
-  );
-
-  const appointment = await createAppointmentService.execute({
-    date: parsedDate,
-    provider_id,
-  });
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,55 +1,23 @@
-import { User } from '@modules/users/infra/typeorm/entities/User';
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import UsersController from '../controllers/UsersController';
+import UsersAvatarController from '../controllers/UsersAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
-usersRouter.post('/', async (request, response) => {
-  const usersRepository = new UsersRepository();
-  try {
-    const { name, email, password } = request.body;
-
-    const createUserService = new CreateUserService(usersRepository);
-
-    const user = await createUserService.execute({
-      name,
-      email,
-      password,
-    });
-
-    delete user.password;
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ erro: err.message });
-  }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatarService = new UpdateUserAvatarService(
-      usersRepository
-    );
-
-    const user = await updateUserAvatarService.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    delete user.password;
-    return response.json(user);
-  }
+  usersAvatarController.update
 );
 
 export default usersRouter;
