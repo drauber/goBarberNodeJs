de0d557f0f4e3182b6a46d7a19bd7ecb10693993
Douglas Rauber <douglas.rauber@gmail.com>
2020-05-22 07:31:33 -0300

    Teste corrigido

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -71,6 +71,6 @@ describe('UpdateUserAvatar', () => {
     });
 
     expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
-    expect(user.avatar).toBe('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
   });
 });
