5c96667a67636618059edd7467ea2a30e4fda93c
Douglas Rauber <douglas.rauber@gmail.com>
2020-05-28 07:54:10 -0300

    Implemented classToClass to transform json

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -10,8 +11,8 @@ export default class ProfileController {
     const user_id = request.user.id;
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
-    return response.json(user);
+    // delete user.password;
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -29,7 +30,7 @@ export default class ProfileController {
       });
 
       delete user.password;
-      return response.json(user);
+      return response.json(classToClass(user));
     } catch (err) {
       return response.status(400).json({ erro: err.message });
     }
--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -16,7 +17,7 @@ export default class UsersController {
       });
 
       delete user.password;
-      return response.json(user);
+      return response.json(classToClass(user));
     } catch (err) {
       return response.status(400).json({ erro: err.message });
     }
